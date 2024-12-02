548a5719c2dc4f8a30ec49bba264e0735ddfdd8b
Miguel Peixe <miguel.peixe@automattic.com>
2024-12-02 16:17:34 -0300

    test: remove unnecessary assertion

--- tests/unit-tests/test-content-distribution.php
+++ tests/unit-tests/test-content-distribution.php
@@ -31,8 +31,6 @@ class TestContentDistribution extends WP_UnitTestCase {
 	public function test_set_post_distribution_persists_post_hash() {
 		$post = $this->factory->post->create_and_get( [ 'post_type' => 'post' ] );
 		$result = Content_Distribution::set_post_distribution( $post->ID, [ 'https://example.com' ] );
-		$this->assertFalse( is_wp_error( $result ) );
-
 		$config = get_post_meta( $post->ID, Content_Distribution::DISTRIBUTED_POST_META, true );
 
 		// Update the post distribution.
