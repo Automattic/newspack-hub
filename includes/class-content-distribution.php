f2de08c96de0bf1dd28de5abb97d3918670e1367
Miguel Peixe <miguel.peixe@automattic.com>
2024-12-09 18:10:07 -0300

    feat(content-distribution): handle post thumbnail (#157)

--- includes/class-content-distribution.php
+++ includes/class-content-distribution.php
@@ -37,6 +37,11 @@ class Content_Distribution {
 	const POST_UNLINKED_META = 'newspack_network_post_unlinked';
 
 	/**
+	 * Post meta key for linked attachments.
+	 */
+	const ATTACHMENT_META = 'newspack_network_linked_attachment';
+
+	/**
 	 * Initialize this class and register hooks
 	 *
 	 * @return void
@@ -244,15 +249,15 @@ class Content_Distribution {
 			'post_id'   => $post->ID,
 			'config'    => $config,
 			'post_data' => [
-				'title'        => html_entity_decode( get_the_title( $post->ID ), ENT_QUOTES, get_bloginfo( 'charset' ) ),
-				'date_gmt'     => $post->post_date_gmt,
-				'modified_gmt' => $post->post_modified_gmt,
-				'slug'         => $post->post_name,
-				'post_type'    => $post->post_type,
-				'raw_content'  => $post->post_content,
-				'content'      => self::get_processed_post_content( $post ),
-				'excerpt'      => $post->post_excerpt,
-				// @ TODO: Add meta, featured image and taxonomies.
+				'title'         => html_entity_decode( get_the_title( $post->ID ), ENT_QUOTES, get_bloginfo( 'charset' ) ),
+				'date_gmt'      => $post->post_date_gmt,
+				'modified_gmt'  => $post->post_modified_gmt,
+				'slug'          => $post->post_name,
+				'post_type'     => $post->post_type,
+				'raw_content'   => $post->post_content,
+				'content'       => self::get_processed_post_content( $post ),
+				'excerpt'       => $post->post_excerpt,
+				'thumbnail_url' => get_the_post_thumbnail_url( $post->ID, 'full' ),
 			],
 		];
 	}
@@ -340,6 +345,42 @@ class Content_Distribution {
 	}
 
 	/**
+	 * Upload the thumbnail for a linked post.
+	 *
+	 * @param int    $post_id       The linked post ID.
+	 * @param string $thumbnail_url The thumbnail URL.
+	 */
+	protected static function upload_thumbnail( $post_id, $thumbnail_url ) {
+		$post_payload         = get_post_meta( $post_id, self::POST_PAYLOAD_META, true );
+		$current_thumbnail_id = get_post_thumbnail_id( $post_id );
+
+		// Bail if the post has a thumbnail and the thumbnail URL is the same.
+		if (
+			$current_thumbnail_id &&
+			$post_payload &&
+			$post_payload['post_data']['thumbnail_url'] === $thumbnail_url
+		) {
+			return;
+		}
+
+		if ( ! function_exists( 'media_sideload_image' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/media.php';
+			require_once ABSPATH . 'wp-admin/includes/file.php';
+			require_once ABSPATH . 'wp-admin/includes/image.php';
+		}
+
+		$attachment_id = media_sideload_image( $thumbnail_url, $post_id, '', 'id' );
+		if ( is_wp_error( $attachment_id ) ) {
+			Debugger::log( 'Failed to upload featured image for post ' . $post_id . ' with message: ' . $attachment_id->get_error_message() );
+			return;
+		}
+
+		update_post_meta( $attachment_id, self::ATTACHMENT_META, true );
+
+		set_post_thumbnail( $post_id, $attachment_id );
+	}
+
+	/**
 	 * Insert a linked post given a distributed post payload.
 	 *
 	 * @param array $post_payload The post payload.
@@ -418,6 +459,18 @@ class Content_Distribution {
 		// The wp_insert_post() function might return `0` on failure.
 		if ( ! $post_id ) {
 			return new WP_Error( 'insert_error', __( 'Error inserting post.', 'newspack-network' ) );
+		}
+
+		// Handle payload thumbnail.
+		$thumbnail_url = $post_payload['post_data']['thumbnail_url'];
+		if ( $thumbnail_url ) {
+			self::upload_thumbnail( $post_id, $thumbnail_url );
+		} elseif ( $linked_post ) {
+			// Delete thumbnail for existing post if it's not set in the payload.
+			$current_thumbnail_id = get_post_thumbnail_id( $post_id );
+			if ( $current_thumbnail_id ) {
+				delete_post_thumbnail( $post_id );
+			}
 		}
 
 		update_post_meta( $post_id, self::POST_PAYLOAD_META, $post_payload );
