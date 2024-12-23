4a71b7db8299b7d5e461278d00bf8bb920865e0c
Miguel Peixe <miguel.peixe@automattic.com>
2024-12-23 17:29:24 -0300

    fix: log error and disable button on click

--- includes/hub/admin/js/event-log.js
+++ includes/hub/admin/js/event-log.js
@@ -8,11 +8,16 @@
       button.addEventListener( 'click', function( ev ) {
         ev.preventDefault();
         button.textContent = newspackNetworkEventLogLabels.copying;
+        button.disabled = true;
         navigator.clipboard.writeText( text ).then( function() {
           button.textContent = newspackNetworkEventLogLabels.copied;
           setTimeout( function() {
             button.textContent = newspackNetworkEventLogLabels.copy;
+            button.disabled = false;
           }, 1000 );
+        } ).catch( function( err ) {
+          console.error( 'Failed to copy: ', err );
+          button.disabled = false;
         } );
       } );
     } );
